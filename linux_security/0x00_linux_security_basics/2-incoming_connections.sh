cb624f9f91be7b21be2177bf98a15d69a75ea6ce
ulvialv <ulvialiyevgithub@gmail.com>
2025-12-26 20:04:13 +0000

    asd

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# IPv4 rulesı
-sudo iptables -P INPUT DROP
-sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -i lo -j ACCEPT
-
-# IPv6 rules ı
-sudo ip6tables -P INPUT DROP
-sudo ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo ip6tables -A INPUT -i lo -j ACCEPT
-
-echo "Rules updated"
-echo "Rules updated (v6)"
