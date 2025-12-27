186a41d09fbba9282f5892edb69d74fe20ff89a5
ulvialv <ulvialiyevgithub@gmail.com>
2025-12-27 10:56:25 +0000

    asd

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-# domain değişkeni
-domain=$1
-
-# subfinder ile subdomainleri çek, her satırı host olarak al
-subfinder -d "$domain" -silent | while read host; do
-    # her host için IP çözümlemesi
-    ip=$(dig +short "$host" | head -n1)
-    # host,IP formatında dosyaya yaz
-    echo "$host,$ip"
-done > "$domain".txt
+subfinder -d $1 -silent | while read h; do echo "$h,$(dig +short $h | head -n1)"; done > $1.txt
