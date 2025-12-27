f2319bfcd2f0f2999792671862c42d2f499e3746
ulvialv <ulvialiyevgithub@gmail.com>
2025-12-27 10:13:05 +0000

    asd

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{k=$1;v=$2; if(k~/(Street)/) print k", "v" "; else if(k~/(Phone Ext|Fax Ext)/) print k":, "; else print k", "v}' > "$1.csv"
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{k=$1;v=$2; if(k~/(Phone Ext|Fax Ext)/) print k", "; else print k", "v}' > "$1.csv"
