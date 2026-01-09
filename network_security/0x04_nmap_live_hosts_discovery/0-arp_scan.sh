ae04b4688bd263418fea4eae9b43eabedeefd956
ulvialv <ulvialiyevgithub@gmail.com>
2026-01-09 11:42:03 +0000

    asd

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# Check if a subnet argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <subnet>"
-    exit 1
-fi
-
-SUBNET="$1"
-
-# Run ARP scan using nmap (host discovery only, no port scan)
 sudo nmap -sn -PR "$SUBNET"
