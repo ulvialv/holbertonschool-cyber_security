806ca41789f6ff449260c2395045cc1f113ded7b
ulvialv <ulvialiyevgithub@gmail.com>
2026-01-18 13:49:39 +0000

    asd

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
-
-if [ -z "$1" ]; then
-  exit 1
-fi
-
-HASH=$(echo "$1" | sed 's/^{xor}//')
-DECODED=$(echo "$HASH" | base64 -d 2>/dev/null)
-KEY=90
-
-RESULT=""
-
-echo "$DECODED" | od -An -t u1 | while read -r c; do
-  RESULT=$(printf "%s%s" "$RESULT" "$(printf "\\$(printf '%03o' $((c ^ KEY)))")")
-done
-
-printf "%s\n" "$RESULT"
+[ -z "$1" ] && exit 1
+printf '%s' "$1" | sed 's/^{xor}//' | base64 -d | awk '{
+  for (i = 1; i <= length($0); i++)
+    printf "%c", xor(ord(substr($0,i,1)), 90)
+  print ""
+}
+function ord(c){return index("\
+\1\2\3\4\5\6\7\10\11\12\13\14\15\16\17\
+\20\21\22\23\24\25\26\27\30\31\32\33\34\35\36\37\
+ !\"#$%&'\''()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",c)-1}
+function xor(a,b){return a^b}'
