0452fa75cc61405fb89470c9d4977879c9ded5e1
ulvialv <ulvialiyevgithub@gmail.com>
2026-01-18 13:35:40 +0000

    asd

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# Arg check
 if [ -z "$1" ]; then
   exit 1
 fi
 
+# Remove {xor} prefix
 HASH="${1#\{xor\}}"
 
+# Base64 decode
 DECODED=$(echo "$HASH" | base64 -d 2>/dev/null)
 
+# XOR decode (WebSphere uses 0x5A)
 RESULT=""
+
 for (( i=0; i<${#DECODED}; i++ )); do
   char=$(printf "%d" "'${DECODED:$i:1}")
   RESULT+=$(printf "\\$(printf '%03o' $((char ^ 0x5A)))")
 done
 
+# Output
 echo "$RESULT"
