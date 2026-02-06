2df0cb504d5996e64b51f56a5f8fe38e399b1e61
ulvialv <ulvialiyevgithub@gmail.com>
2026-02-06 13:30:46 -0500

    ad

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-# Check if a password argument is provided
-if [ -z "$1" ]; then
-    exit 1
-fi
-
-# Hash the password using SHA-1 and store the result
-echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
+echo -n "$1" | sha1sum | cut -d' ' -f1 > 0_hash.txt
