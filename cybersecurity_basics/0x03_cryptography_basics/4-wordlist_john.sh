9ff77aeaaccbda2b8bba7c34800c74f5cf01ca9b
ulvialv <ulvialiyevgithub@gmail.com>
2026-02-06 13:38:46 -0500

    asd

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | cut -d':' -f2 > 4-password.txt
+john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show --format=raw-md5 "$1" | cut -d':' -f2 > 4-password.txt
