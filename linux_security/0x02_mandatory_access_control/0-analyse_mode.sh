8b86c9ed7be15e602ca0c4a5351d1e40828b2213
ulvialv <ulvialiyevgithub@gmail.com>
2026-02-11 15:31:54 +0400

    asd

--- linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
+++ linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# SELinux kurulu mu kontrol et
-if command -v getenforce &> /dev/null; then
-    MODE=$(getenforce)
-
-    # Çıktıyı küçük harfe çeviriyoruz (örnek çıktıyla uyumlu olsun diye)
-    MODE_LOWER=$(echo "$MODE" | tr '[:upper:]' '[:lower:]')
-
-    echo "SELinux status:                 $MODE_LOWER"
-else
-    echo "SELinux status:                 disabled"
-fi
+getenforce 2>/dev/null || echo "disabled"
