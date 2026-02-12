15a6f7703ee91fd33f33ed650efac697ae348fb7
ulvialv <ulvialiyevgithub@gmail.com>
2026-02-12 18:37:51 +0400

    asd

---
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+iptables -P INPUT DROP
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
