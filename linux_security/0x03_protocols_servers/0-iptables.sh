9fc75b9bb0c723cb74afb770f9c7a5e750157e6a
ulvialv <ulvialiyevgithub@gmail.com>
2026-02-12 18:25:01 +0400

    asd

--- linux_security/0x03_protocols_servers/0-iptables.sh
+++ linux_security/0x03_protocols_servers/0-iptables.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Tüm tabloları (filter, nat, mangle, raw, security) line number ile göster
-
-tables=("filter" "nat" "mangle" "raw" "security")
-
-for table in "${tables[@]}"; do
-    echo "=============================="
-    echo "Table: $table"
-    echo "=============================="
-    sudo iptables -t $table -L -v -n --line-numbers 2>/dev/null
-    echo ""
-done
+iptables -L -v -n --line-numbers
