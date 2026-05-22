79822b5ae09ec1b19351394ecdcf8bbfd6c03d17
ulvialv <ulvialiyevgithub@gmail.com>
2026-05-22 13:11:44 +0400

    asd

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+def post_request(url, body_params)
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = (uri.scheme == 'https')
+
+  request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+  request.body = body_params.to_json
+
+  response = http.request(request)
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(response.body))
+end
