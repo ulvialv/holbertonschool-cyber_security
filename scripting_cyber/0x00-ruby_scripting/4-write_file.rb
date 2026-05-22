c04923884cda3346c6207a574877817cbf71cdab
ulvialv <ulvialiyevgithub@gmail.com>
2026-05-22 13:02:32 +0400

    asd

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -4,11 +4,10 @@ def merge_json_files(file1_path, file2_path)
   data1 = JSON.parse(File.read(file1_path))
   data2 = JSON.parse(File.read(file2_path))
   
-  merged_data = data1 + data2
+  merged_data = data2 + data1
   
   File.open(file2_path, 'w') do |f|
     f.write(JSON.pretty_generate(merged_data))
   end
   puts "Merged JSON written to file.json"
-end 
-  
+end
