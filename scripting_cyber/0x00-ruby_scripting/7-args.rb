edde7ec882bb52806e4e5b5ca449804757c07222
ulvialv <ulvialiyevgithub@gmail.com>
2026-05-22 13:09:10 +0400

    asd

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,9 +1,6 @@
 def print_arguments
-  if ARGV.empty?
-    puts "No arguments provided."
-  else
-    ARGV.each_with_index do |arg, index|
-      puts "#{index + 1}. #{arg}"
-    end
+  puts "Arguments:"
+  ARGV.each do |arg|
+    puts arg
   end
 end
