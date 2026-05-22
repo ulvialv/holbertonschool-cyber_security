6b92422c6f26d1fdd579fe7b9ad3e7508312aa1f
ulvialv <ulvialiyevgithub@gmail.com>
2026-05-22 12:58:33 +0400

    asd

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,17 @@
+require 'json'
+
+def count_user_ids(path)
+  file = File.read(path)
+  data = JSON.parse(file)
+  
+  counts = Hash.new(0)
+  
+  data.each do |item|
+    user_id = item['userId']
+    counts[user_id] += 1
+  end
+  
+  counts.each do |user_id, count|
+    puts "#{user_id}: #{count}"
+  end
+end
