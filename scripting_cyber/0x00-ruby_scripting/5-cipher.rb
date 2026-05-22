9a3bd9006a2cd8f2513f564c0bedd31b420ab635
ulvialv <ulvialiyevgithub@gmail.com>
2026-05-22 13:03:21 +0400

    asd

---
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -0,0 +1,27 @@
+class CaesarCipher
+  def initialize(shift)
+    @shift = shift
+  end
+
+  def encrypt(message)
+    cipher(message, @shift)
+  end
+
+  def decrypt(message)
+    cipher(message, -@shift)
+  end
+
+  private
+
+  def cipher(message, shift)
+    message.chars.map do |char|
+      if char =~ /[a-z]/
+        ((char.ord - 'a'.ord + shift) % 26 + 'a'.ord).chr
+      elsif char =~ /[A-Z]/
+        ((char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
+      else
+        char
+      end
+    end.join
+  end
+end
